eaec8108c3e0019eebbbac46b14fab767150120c
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-10-06 10:44:59 -0300

    MVC and MyLabel

---
+++ Memory Puzzle/Model/MyLabel.swift
@@ -0,0 +1,17 @@
+//
+//  MyLabel.swift
+//  Memory Puzzle
+//
+//  Created by Livia Carvalho Keller on 06/10/22.
+//
+
+import UIKit
+
+class MyLabel: UILabel {
+
+    var internalNum: Int!
+    
+    static var question = "?"
+    static var smile = "☺️"
+    
+}
